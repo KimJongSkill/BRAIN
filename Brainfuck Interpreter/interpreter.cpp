f5ec6b72f5cfa76322ae4724eb0f7b9b324af907
KimJongSkill <antroseconomides@hotmail.com>
2015-09-12 16:01:51 +0300

    Use std::vector::empty to determine which Multiplication mode to use and do nothing on a PopFast

--- Brainfuck Interpreter/interpreter.cpp
+++ Brainfuck Interpreter/interpreter.cpp
@@ -40,7 +40,6 @@ void Instruction::Execute() const
 		*Parent->DataPointer = 0;
 		break;
 	case Type::PopFast:
-		Parent->FastStorage = 0;
 		break;
 	case Type::Reset:
 		std::fill_n(Parent->DataPointer, Data[0], 0);
@@ -48,10 +47,10 @@ void Instruction::Execute() const
 		break;
 	case Type::Multiplication:
 		std::advance(Parent->DataPointer, Data[0]);
-		
-		if (Parent->FastStorage)
+
+		if (Parent->Storage.empty())
 			*Parent->DataPointer += Data[1] * Parent->FastStorage;
-		else if (!Parent->Storage.empty())
+		else
 			*Parent->DataPointer += std::accumulate(std::cbegin(Parent->Storage), std::cend(Parent->Storage), Data[1], std::multiplies<value_type>());
 		break;
 	case Type::Seek:
